592896693f07a5123fe11ad8ad054a9a50cb3f90
Muhammad Fauzan <muhammadfauzanfadhlulbarr@gmail.com>
2020-10-19 10:44:21 +0700

    first fommit

---
+++ Latihan4.java
@@ -0,0 +1,17 @@
+/**
+ * Latihan4
+ */
+public class Latihan4 {
+    public void tambahKambing() {
+        int jumlahkambing = 0;
+        
+        jumlahkambing = jumlahkambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah = " + jumlahkambing);
+        
+    }
+    
+    public static void main(String[] args) {
+        Latihan4 kambingjantan = new Latihan4();
+        kambingjantan.tambahKambing();
+    }
+}
